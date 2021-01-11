5dc3b5734aa6c05d835e2fb65a0aaec903863d16
Antoine <antoine.brld@gmail.com>
2021-01-11 18:35:57 +0100

    backend

---
+++ server/database/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    CONNECT_STRING: "mongodb+srv://ircmern:rcmvfAu7F4XRIbkV@cluster0.iroiu.mongodb.net/ircmern?authSource=admin&replicaSet=atlas-kj11q3-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+};
