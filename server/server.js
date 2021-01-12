b267fe02d6b16d7e9579b56d86a99ce9358d42b1
Oddwerth <lucasmichalet81@gmail.com>
2021-01-12 14:00:02 +0100

    Socketio debut

--- server/server.js
+++ server/server.js
@@ -1,25 +1,48 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const socketio = require("socket.io");
 const app = express();
 
 const corsOptions = {
     origin: "http://localhost:3000"
 };
-app.use(cors(corsOptions));
 
+const server = http.createServer(app);
+
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+
+app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}));
 
+//Run quand qqn se connecte
+io.on('connection', socket => {
+    console.log('New WebSocket connection');
+    socket.emit('message', "Bonjour espèce de beau gosse")
+});
+
+
 // Connect to Database
-require("./database")
+require("./database");
 
 // Use routes
-require("./routers")(app)
+require("./routers")(app);
+
+//Socket IO
+require("./socketio");
+
 
 // Start listening
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
