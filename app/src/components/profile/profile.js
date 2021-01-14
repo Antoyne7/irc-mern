de63d99ce99d8da70ae971e30f6190525e30c854
Antoine <antoine.brld@gmail.com>
2021-01-14 20:51:21 +0100

    save

--- app/src/components/profile/profile.js
+++ app/src/components/profile/profile.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import "./profile.style.scss"
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTrash} from '@fortawesome/free-solid-svg-icons'
+import {faTrash, faEdit} from '@fortawesome/free-solid-svg-icons'
 
 import Navigation from "../navigation/navigation";
 import Button from "../button/button";
 import Input from "../input/input"
 
 const Profile = () => {
+    const pictureInput = useRef()
+
+    const [picture, setPicture] = useState(null);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordRepeat, setPasswordRepeat] = useState("");
+    const [oldpassword, setOldPassword] = useState("");
+
+    const onChangeUsername = (e) => {
+        const username = e.target.value;
+        setUsername(username);
+    };
+
+    const onChangePassword = (e) => {
+        const password = e.target.value;
+        setPassword(password);
+    };
+
+    const onChangePasswordRepeat = (e) => {
+        const passwordRepeat = e.target.value;
+        setPasswordRepeat(passwordRepeat);
+    };
+
+    const onChangeOldPassword = (e) => {
+        const oldPassword = e.target.value;
+        setOldPassword(oldPassword);
+    };
 
     return (
         <div className="Profile">
@@ -25,8 +52,14 @@ const Profile = () => {
                     <FontAwesomeIcon icon={faTrash} color={"var(--red)"}/>
                 </div>
 
-                <div className="profile-picture">
-
+                <div className="profile-picture-container">
+                    <div className="profile-picture">
+                        <img src="" alt=""/>
+                    </div>
+                    <label htmlFor="profile-picture">
+                        <FontAwesomeIcon icon={faEdit} size="2x" color="var(--contrast-projet)" />
+                    </label>
+                    <input ref={pictureInput} id="profile-picture" type="file" />
                 </div>
 
                 <Input
